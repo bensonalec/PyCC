0233dfb4245f2b91a308501ac8faae201c850a2e
Alec Benson <alexander.benson@student.nmt.edu>
2020-02-03 14:22:18 -0700

    Added boilerplate for ast and parser

---
+++ frontend/ast.py
@@ -0,0 +1,4 @@
+class ASTNode:
+	def __init__(self,content,token):
+		self.content = content
+		self.token = token
---
+++ frontend/parser.py
@@ -0,0 +1,11 @@
+class Parser:
+	def __init__(self):
+		self.head = None
+
+	def parse(self):
+		pass
+	
+	def getHead(self):
+		return self.head
+
+	
